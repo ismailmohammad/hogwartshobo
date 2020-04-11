2d64ad9f1483f7d2ba5b73dccfa618d07a2632fb
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2020-04-11 04:40:11 -0400

    Change benchmark method, allowing for easier testing, TODO: fs, msg response, randomization

--- benchmarker.py
+++ benchmarker.py
@@ -1,13 +1,13 @@
 import timeit
 
-trials = 3
+trials = 5
 
-for i in range (4,7):
+for i in range (0,2):
     # Open benchmark file
     benchmark_file = open('tests/benchmarked.txt', 'a')
 
-    time = timeit.timeit('game(True, '+ str(i) +')', setup="from hobo import game" ,number=trials) 
+    time = timeit.timeit('os.system("python hobo.py b '+ str(i) +'")', setup="import os", number=trials) 
     # Append 'hello' at the end of file
-    benchmark_file.write("Number of other hobos: " + str(i) + "Trials: " + str(trials) + " Average Time of Longest surviving Hobo: " + str(time) + "\n")
+    benchmark_file.write("Number of other hobos: " + str(i) + " Trials: " + str(trials) + "Longest surviving Hobo: " + str(time) + "\n")
     # Close the file
     benchmark_file.close()
